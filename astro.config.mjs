c647ebbcc4ef1364375df5be50e0356aa4cc2b8f
Stron <luisstron45@gmail.com>
2025-03-07 17:19:33 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ const LIVE_URL = "https://github.com/adolfin07/AdolfoZepeda.github.io"
 // https://astro.build/config
 export default defineConfig({
   site: "https://adolfozepeda.github.io", // Reemplaza con tu URL correcta
-  base: "/AdolfoZepeda.github.io/", // Ajusta el base si es necesario
-
 });
 
 
